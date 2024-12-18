96637350c5655baa5ddedbb87445ca7861368b94
Alexis <AlexDdevs@gmail.com>
2024-12-18 09:43:09 +0100

    🚚 Put database in a folder

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,9 +2,8 @@ import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
     type: "sqlite",
-    database: "tgc.sqlite",
+    database: "database/tgc.sqlite",
     entities: ["./src/entities/*.ts"],
     synchronize: true,
-    logging: true
-  });
-  
+    logging: true,
+});
