e785539af26863283a4e198823597cc6b085ee00
Alexis <AlexDdevs@gmail.com>
2025-01-14 20:51:38 +0100

    ✨ Use context and authChecker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,20 +6,36 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { AdsResolver } from "./resolvers/Ads";
 import { CategoriesResolver } from "./resolvers/Categories";
 import { TagsResolver } from "./resolvers/Tags";
+import { UserResolver } from "./resolvers/User";
+import { authChecker } from "./auth";
 
 const initialize = async () => {
     await dataSource.initialize();
     console.log("DataSource is connected");
 
     const schema = await buildSchema({
-        resolvers: [AdsResolver, CategoriesResolver, TagsResolver],
+        resolvers: [
+            AdsResolver,
+            CategoriesResolver,
+            TagsResolver,
+            UserResolver,
+        ],
+        authChecker,
     });
 
     const server = new ApolloServer({ schema });
 
     const { url } = await startStandaloneServer(server, {
         listen: { port: 5000 },
+
+        context: async ({ req, res }) => {
+            return {
+                req,
+                res,
+            };
+        },
     });
+
     console.log(`GraphQL server ready at ${url}`);
 };
 
