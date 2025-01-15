487822af6e6f91f74444f8c795e204094f10c89b
Alexis <AlexDdevs@gmail.com>
2025-01-15 11:41:56 +0100

    🎨 Create a fonction to get User and can use it everywhere

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,24 +1,56 @@
-import { decode, verify } from "jsonwebtoken";
+import { verify } from "jsonwebtoken";
 import Cookies from "cookies";
 import { AuthChecker } from "type-graphql";
+import { User } from "./entities/User";
 
-export const authChecker: AuthChecker<{ req: any; res: any }> = (
-    { root, args, context, info },
-    roles
-) => {
+export type ContextType = {
+    req: any;
+    res: any;
+    user: User | null | undefined;
+};
+
+export type AuthContextType = ContextType & { user: User };
+
+export const getUserFromContext = async (
+    context: ContextType
+): Promise<User | null> => {
     const cookies = new Cookies(context.req, context.res);
     const token = cookies.get("token");
 
     if (!token) {
         console.log("Missing token in cookies");
-        return false;
+        return null;
     }
 
     try {
-        verify(token, process.env.JWT_SECRET_KEY);
+        const payload = verify(
+            token,
+            process.env.JWT_SECRET_KEY
+        ) as unknown as { id: number };
+
         console.log("Access authorized");
+
+        const user = await User.findOneBy({
+            id: payload.id,
+        });
+
+        return user;
+    } catch (err) {
+        console.error(err);
+        throw new Error("Invalid JWT");
+    }
+};
+
+export const authChecker: AuthChecker<ContextType> = async (
+    { root, args, context, info },
+    roles
+) => {
+    const user = await getUserFromContext(context);
+    context.user = user;
+
+    if (user) {
         return true;
-    } catch (error) {
-        console.log("Access denied");
+    } else {
+        return false;
     }
 };
