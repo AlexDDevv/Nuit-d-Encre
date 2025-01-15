2e824069101292da66bf984ac5a3b0538ed01510
Alexis <AlexDdevs@gmail.com>
2025-01-15 14:48:42 +0100

    🛂 Authorized ads for connected users

--- backend/src/resolvers/Ads.ts
+++ backend/src/resolvers/Ads.ts
@@ -1,5 +1,14 @@
-import { Arg, Mutation, Query, Resolver, ID } from "type-graphql";
+import {
+    Arg,
+    Mutation,
+    Query,
+    Resolver,
+    ID,
+    Authorized,
+    Ctx,
+} from "type-graphql";
 import { Ad, createAdInput, updateAdInput } from "../entities/Ad";
+import { AuthContextType } from "../auth";
 
 @Resolver()
 export class AdsResolver {
@@ -32,22 +41,31 @@ export class AdsResolver {
         }
     }
 
+    @Authorized()
     @Mutation(() => Ad)
     async createAd(
-        @Arg("data", () => createAdInput) data: createAdInput
+        @Arg("data", () => createAdInput) data: createAdInput,
+        @Ctx() context: AuthContextType
     ): Promise<Ad> {
         const newCategory = new Ad();
-        Object.assign(newCategory, data);
+        const user = context.user;
+        Object.assign(newCategory, data, { createdBy: user });
         await newCategory.save();
         return newCategory;
     }
 
+    @Authorized()
     @Mutation(() => Ad, { nullable: true })
     async updateAd(
         @Arg("id", () => ID) id: number,
-        @Arg("data", () => updateAdInput) data: updateAdInput
+        @Arg("data", () => updateAdInput) data: updateAdInput,
+        @Ctx() context: AuthContextType
     ): Promise<Ad | null> {
-        const ad = await Ad.findOneBy({ id });
+        const ad = await Ad.findOneBy({
+            id,
+            createdBy: { id: context.user.id },
+        });
+
         if (ad !== null) {
             Object.assign(ad, data);
             await ad.save();
@@ -58,8 +76,14 @@ export class AdsResolver {
     }
 
     @Mutation(() => Ad, { nullable: true })
-    async deleteAd(@Arg("id", () => ID) id: number): Promise<Ad | null> {
-        const ad = await Ad.findOneBy({ id });
+    async deleteAd(
+        @Arg("id", () => ID) id: number,
+        @Ctx() context: AuthContextType
+    ): Promise<Ad | null> {
+        const ad = await Ad.findOneBy({
+            id,
+            createdBy: { id: context.user.id },
+        });
         if (ad !== null) {
             await ad.remove();
             Object.assign(ad, { id });
