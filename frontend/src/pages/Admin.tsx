0275c4a053950cdb9b2dc3d5b55a3a48874ef3a9
Alexis <AlexDdevs@gmail.com>
2025-01-25 20:55:09 +0100

    💄 Update admin page

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,21 +1,21 @@
-import styled from "styled-components"
-import AppStats from "../components/AppStats"
-import AdminPanel from "../components/AdminPanel"
-import { IdCard, User, Tag } from "lucide-react"
-import data from "../data/data.json"
+import styled from "styled-components";
+import AppStats from "../components/AppStats";
+import AdminPanel from "../components/AdminPanel";
+import { IdCard, User, Tag } from "lucide-react";
+import data from "../data/data.json";
 
 const SectionAdmin = styled.section`
     display: flex;
     flex-direction: column;
     gap: 75px;
-`
+`;
 
 const StatsContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
     gap: 20px;
-`
+`;
 
 export default function Admin() {
     const icons = [IdCard, User, Tag];
@@ -37,5 +37,5 @@ export default function Admin() {
             </StatsContainer>
             <AdminPanel />
         </SectionAdmin>
-    )
+    );
 }
