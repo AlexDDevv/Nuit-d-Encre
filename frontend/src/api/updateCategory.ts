0d459c8d8dfc953d87ecac70a578c57dc6d58b7c
Alexis <AlexDdevs@gmail.com>
2025-01-26 12:08:48 +0100

    ✨ Create the mutation to update a category

--- frontend/src/api/updateCategory.ts
+++ frontend/src/api/updateCategory.ts
@@ -1,10 +1,9 @@
 import { gql } from "@apollo/client";
 
-export const createCategory = gql`
-    mutation Mutation($data: updateCategoryInput!) {
-        createCategory(data: $data) {
+export const updateCategory = gql`
+    mutation updateCategory($data: CategoryUpdateInput!, $id: ID!) {
+        updateCategory(data: $data, id: $id) {
             id
-            name
         }
     }
 `;
