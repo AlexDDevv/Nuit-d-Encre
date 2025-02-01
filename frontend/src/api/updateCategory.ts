8d0531ca14cc9ebadcf24401a4a5ce93445a280e
Alexis <AlexDdevs@gmail.com>
2025-02-01 08:50:31 +0100

    🐛 Wrong name for InputType

--- frontend/src/api/updateCategory.ts
+++ frontend/src/api/updateCategory.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const updateCategory = gql`
-    mutation updateCategory($data: CategoryUpdateInput!, $id: ID!) {
+    mutation updateCategory($data: updateCategoryInput!, $id: ID!) {
         updateCategory(data: $data, id: $id) {
             id
         }
