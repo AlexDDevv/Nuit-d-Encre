510877922825bf7db7f9750bd56ff0803748ce07
Alexis <AlexDdevs@gmail.com>
2025-02-01 08:54:25 +0100

    🎨 Better structure for main component

--- frontend/src/components/AdminPanel.tsx
+++ frontend/src/components/AdminPanel.tsx
@@ -2,19 +2,27 @@ import { useState } from "react";
 import {
     AdminPanelContainer,
     PanelContainer,
-    TabsContainer,
-    Tab,
-    TabName,
+    ManageContainer,
+    AdsContainer,
+    NoAd,
 } from "../components/styled/PanelAdmin.styles";
-import data from "../data/data.json";
 import ManageAds from "./ManageContent/ManageAds";
 import AdCard from "./AdCard";
 import { useQuery } from "@apollo/client";
 import { AdTypeCard } from "../../types";
 import { queryAds } from "../api/ads";
+import Tabs from "./Tabs";
+import ManageCategories from "./ManageContent/ManageCategories";
+import CategoryForm from "./ManageContent/CategoryForm";
 
 export default function AdminPanel() {
+    const [selectedTab, setSelectedTab] = useState(0);
     const [previewAdId, setPreviewAdId] = useState<number | null>(null);
+    const [previewAdsId, setPreviewAdsId] = useState<number[] | null>(null);
+    const [showCategoryForm, setShowCategoryForm] = useState(false);
+    const [editingCategoryId, setEditingCategoryId] = useState<
+        number | undefined
+    >();
 
     const { data: dataAds } = useQuery<{ ads: AdTypeCard[] }>(queryAds, {
         fetchPolicy: "cache-and-network",
@@ -25,21 +33,82 @@ export default function AdminPanel() {
         setPreviewAdId(id);
     };
 
+    const handlePreviewAdsChange = (ids: number[] | null) => {
+        setPreviewAdsId(ids);
+        setShowCategoryForm(false);
+    };
+
+    const handleCategoryForm = (categoryId?: number) => {
+        setShowCategoryForm(!showCategoryForm);
+        setEditingCategoryId(categoryId);
+        setPreviewAdsId(null);
+    };
+
+    const handleChangeTab = (index: number) => {
+        setSelectedTab(index);
+    };
+
     return (
         <AdminPanelContainer>
             <PanelContainer>
-                <TabsContainer>
-                    {data.tabs.map((tab, i) => (
-                        <Tab key={i}>
-                            <TabName>{tab.tab}</TabName>
-                        </Tab>
-                    ))}
-                </TabsContainer>
-                <ManageAds onPreviewAdChange={handlePreviewAdChange} />
+                <Tabs handleChangeTab={handleChangeTab} />
+                <ManageContainer>
+                    {selectedTab === 0 && (
+                        <>
+                            <ManageAds
+                                onPreviewAdChange={handlePreviewAdChange}
+                            />
+                            {previewAdId && ads && (
+                                <AdCard
+                                    {...ads.find(
+                                        (ad) => ad.id === previewAdId
+                                    )!}
+                                />
+                            )}
+                        </>
+                    )}
+                    {selectedTab === 1 && (
+                        <>
+                            <ManageCategories
+                                onPreviewAdChange={handlePreviewAdsChange}
+                                showCategoryForm={handleCategoryForm}
+                            />
+                            {previewAdsId &&
+                                ads &&
+                                (ads.filter((ad) =>
+                                    previewAdsId.includes(ad.id)
+                                ).length > 0 ? (
+                                    <AdsContainer>
+                                        {previewAdsId.map((id) => {
+                                            const ad = ads.find(
+                                                (ad) => ad.id === id
+                                            );
+                                            return ad ? (
+                                                <AdCard {...ad} key={id} />
+                                            ) : null;
+                                        })}
+                                    </AdsContainer>
+                                ) : (
+                                    <NoAd>
+                                        Il n'y a pas encore d'annonce pour cette
+                                        catégorie.
+                                    </NoAd>
+                                ))}
+                        </>
+                    )}
+                    {showCategoryForm && (
+                        <CategoryForm
+                            onCategoryCreated={async () => {
+                                setShowCategoryForm(false);
+                            }}
+                            onCategoryUpdated={async () => {
+                                setShowCategoryForm(false);
+                            }}
+                            editingId={editingCategoryId}
+                        />
+                    )}
+                </ManageContainer>
             </PanelContainer>
-            {previewAdId && ads && (
-                <AdCard {...ads.find((ad) => ad.id === previewAdId)!} />
-            )}
         </AdminPanelContainer>
     );
 }
