db8c0497db4114ab631209ab0d733f5fe266e6c4
Alexis <AlexDdevs@gmail.com>
2025-02-01 19:03:02 +0100

    💄 Use the prop to change background of selected tab

--- frontend/src/components/Tabs.tsx
+++ frontend/src/components/Tabs.tsx
@@ -1,15 +1,19 @@
 import { Tab, TabName, TabsContainer } from "./styled/PanelAdmin.styles";
 import data from "../data/data.json";
 
+interface TabsProps {
+    handleChangeTab: (index: number) => void;
+    isSelected: number
+}
+
 export default function Tabs({
     handleChangeTab,
-}: {
-    handleChangeTab: (index: number) => void;
-}) {
+    isSelected
+}: TabsProps) {
     return (
         <TabsContainer>
             {data.tabs.map((tab, i) => (
-                <Tab key={i} onClick={() => handleChangeTab(i)}>
+                <Tab key={i} onClick={() => handleChangeTab(i)} background={isSelected === i ? "var(--border)" : "var(--card)"}>
                     <TabName>{tab.tab}</TabName>
                 </Tab>
             ))}
