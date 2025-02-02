808182bca609e56b041388f46d72a3c13d505f1c
Alexis <AlexDdevs@gmail.com>
2025-02-02 16:40:55 +0100

    ✨ Add toasts and errors for sign up form

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -10,21 +10,37 @@ import {
     AlreadyHaveAccount,
 } from "../components/styled/Form.styles";
 import { Button } from "../components/StyledButton";
-import ErrorForm from "../components/ErrorForm";
 import { Link, useNavigate } from "react-router-dom";
 import { createUser } from "../api/createUser";
 import { useMutation } from "@apollo/client";
+import { useToast } from "../components/Toaster/ToasterHook";
 
 export default function SignUp() {
     const [email, setEmail] = useState("alex@gmail.com");
     const [password, setPassword] = useState("Supersecret!1");
     const [confirmPassword, setConfirmPassword] = useState("Supersecret!1");
-    const [error, setError] = useState("");
+    const [error, setError] = useState<boolean>(false);
+    const { addToast } = useToast();
     const navigate = useNavigate();
 
     const [doCreateUser] = useMutation(createUser);
 
     const doSubmit = async () => {
+        const requiredFields = [email, password, confirmPassword];
+
+        const isFormValid = requiredFields.every(
+            (field) => field && (Array.isArray(field) ? field.length > 0 : true)
+        );
+
+        if (!isFormValid) {
+            addToast(
+                "Veuillez remplir tous les champs du formulaire.",
+                "warning"
+            );
+            setError(true);
+            return;
+        }
+
         try {
             await doCreateUser({
                 variables: {
@@ -34,22 +50,27 @@ export default function SignUp() {
                     },
                 },
             });
-
+            setError(false);
+            addToast("Inscription réussie !", "success");
             navigate("/signin", { replace: true });
         } catch (e: any) {
             console.error(e.message);
+            setError(true);
             if (
                 e.message.includes(
                     "Password must be at least 10 characters long and include 1 number, 1 uppercase letter, and 1 symbol"
                 )
             ) {
-                setError("Le mot de passe n'est pas assez fort");
-            } else if (e.message.includes("email must be an email")) {
-                setError("L'email est invalide");
+                addToast("Le mot de passe n'est pas assez fort", "warning");
+            } else if (e.message.includes("Email must be an email")) {
+                addToast("L'email est invalide", "warning");
             } else if (password !== confirmPassword) {
-                setError("Les mots de passe ne sont pas identiques");
+                addToast("Les mots de passe ne sont pas identiques", "error");
             } else {
-                setError("Un compte avec cette adresse email existe déjà");
+                addToast(
+                    "Un compte avec cette adresse email existe déjà",
+                    "error"
+                );
             }
         }
     };
@@ -76,6 +97,7 @@ export default function SignUp() {
                                 placeholder="Ajouter une adresse mail..."
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
+                                error={error}
                             />
                         </InputContainer>
                         <InputContainer>
@@ -86,6 +108,7 @@ export default function SignUp() {
                                 placeholder="Ajouter un mot de passe..."
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                                error={error}
                             />
                         </InputContainer>
                         <InputContainer>
@@ -100,6 +123,7 @@ export default function SignUp() {
                                 onChange={(e) =>
                                     setConfirmPassword(e.target.value)
                                 }
+                                error={error}
                             />
                         </InputContainer>
                     </InputsContainer>
@@ -110,7 +134,6 @@ export default function SignUp() {
                 Vous avez déjà un compte?{" "}
                 <Link to={"/signin"}>Connectez vous!</Link>
             </AlreadyHaveAccount>
-            {error && <ErrorForm error={error} />}
         </>
     );
 }
