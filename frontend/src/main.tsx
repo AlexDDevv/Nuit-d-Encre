ff132a7b106099a200e20e8cc8d31bb70c1e2927
Alexis <AlexDdevs@gmail.com>
2025-02-02 15:04:52 +0100

    ✨ Use the toaster provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ToasterProvider } from "./components/Toaster/ToasterProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ToasterProvider>
+            <App />
+        </ToasterProvider>
+    </StrictMode>
+);
