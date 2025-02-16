294f16a215b6d9439bb32831ba13607b45561614
Alexis <AlexDdevs@gmail.com>
2025-02-16 21:45:01 +0100

    🎨 Edit and improve ActionButton

--- frontend/src/components/UI/ActionButton.tsx
+++ frontend/src/components/UI/ActionButton.tsx
@@ -1,31 +1,31 @@
 import clsx from "clsx";
+import { Link } from "react-router-dom";
 
 interface ActionButtonProps {
-  content: string;
-  bgColor: string;
-  color: string;
-  bgColorHover: string;
-  colorHover: string;
+    bgColor: string;
+    color: string;
+    path?: string;
+    content: string;
+    onClick?: () => void;
 }
 
 export default function ActionButton({
-  content,
-  bgColor,
-  color,
-  bgColorHover,
-  colorHover,
+    bgColor,
+    color,
+    path,
+    content,
+    onClick,
 }: ActionButtonProps) {
-  return (
-    <button
-      className={clsx(
-        bgColor,
-        color,
-        bgColorHover,
-        colorHover,
-        "font-bodyFont flex h-10 items-center justify-center rounded-lg border-0 px-4 py-0 font-semibold transition-colors duration-200 ease-in-out",
-      )}
-    >
-      {content}
-    </button>
-  );
+    return (
+        <button
+            className={clsx(
+                bgColor,
+                color,
+                "flex h-10 cursor-pointer items-center justify-center rounded-lg border-0 px-4 py-0 font-semibold transition-colors duration-200 ease-in-out",
+            )}
+            onClick={onClick}
+        >
+            {path ? <Link to={path}>{content}</Link> : content}
+        </button>
+    );
 }
