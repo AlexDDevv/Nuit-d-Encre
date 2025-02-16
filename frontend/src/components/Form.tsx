93786ef3a5973e83aa0e02b4c7bfa8f139d54102
Alexis <AlexDdevs@gmail.com>
2025-02-16 21:45:26 +0100

    💄 Styled-component to Tailwind

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,64 +1,16 @@
-import styled from "styled-components";
 import { Search } from "lucide-react";
-import { Button } from "./StyledButton";
-
-const ResearchForm = styled.form`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    max-width: 700px;
-    flex-grow: 1;
-    gap: 10px;
-    position: relative;
-`;
-
-const InputForm = styled.input`
-    background-color: var(--input);
-    border: none;
-    border-radius: 6px;
-    color: var(--accent-foreground);
-    font-size: 12px;
-    font-family: inherit;
-    height: 40px;
-    width: 100%;
-    padding: 8px;
-
-    &::placeholder {
-        opacity: 0.85;
-        font-style: italic;
-    }
-
-    &:focus {
-        outline: 2px solid var(--ring);
-    }
-`;
-
-const SearchButton = styled(Button)`
-    position: absolute;
-    right: 8px;
-`;
 
 export default function Form() {
     return (
-        <ResearchForm>
-            <InputForm
+        <form className="relative flex max-w-3xl grow items-center justify-center">
+            <input
+                className="bg-input text-accent-foreground focus:outline-ring h-10 w-full rounded-md border-0 p-2 text-xs italic placeholder:opacity-85 focus:outline-2"
                 type="search"
                 placeholder="Rechercher sur The good corner"
             />
-            <SearchButton
-                background="var(--primary)"
-                rounded="6px"
-                width="30px"
-                height="30px"
-                padding="0"
-                type="button"
-            >
-                <Search
-                    color="var(--primary-foreground)"
-                    height={16}
-                    width={16}
-                />
-            </SearchButton>
-        </ResearchForm>
+            <div className="bg-primary absolute right-2 cursor-pointer rounded-md p-2">
+                <Search className="text-primary-foreground h-4 w-4" />
+            </div>
+        </form>
     );
 }
