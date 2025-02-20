ee322872b629b3f9c9b14c330b7b5b41880128e2
Alexis <AlexDdevs@gmail.com>
2025-02-20 20:01:44 +0100

    🎨 Add props for type and margin

--- frontend/src/components/UI/ActionButton.tsx
+++ frontend/src/components/UI/ActionButton.tsx
@@ -2,33 +2,41 @@ import clsx from "clsx";
 import { Link } from "react-router-dom";
 
 interface ActionButtonProps {
+    type?: "button" | "submit";
     bgColor: string;
     color: string;
     width?: string;
+    margin?: string;
     path?: string;
     content: string;
     onClick?: () => void;
 }
 
 export default function ActionButton({
+    type = "button",
     bgColor,
     color,
     width,
+    margin,
     path,
     content,
     onClick,
 }: ActionButtonProps) {
-    return (
-        <button
-            className={clsx(
-                bgColor,
-                color,
-                width,
-                "flex h-10 cursor-pointer items-center justify-center rounded-lg border-0 px-4 py-0 font-semibold transition-colors duration-200 ease-in-out",
-            )}
-            onClick={onClick}
-        >
-            {path ? <Link to={path}>{content}</Link> : content}
+    const buttonClass = clsx(
+        bgColor,
+        color,
+        width,
+        margin,
+        "flex h-10 cursor-pointer items-center justify-center rounded-lg border-0 px-4 py-0 font-semibold transition-colors duration-200 ease-in-out",
+    );
+
+    return path ? (
+        <Link to={path} className={buttonClass}>
+            {content}
+        </Link>
+    ) : (
+        <button type={type} className={buttonClass} onClick={onClick}>
+            {content}
         </button>
     );
 }
