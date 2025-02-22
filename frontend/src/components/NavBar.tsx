6bc673d24b17239eebd285ef7f41bb55052c45f7
Alexis <AlexDdevs@gmail.com>
2025-02-22 17:44:19 +0100

    💄 Edit links padding

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -17,7 +17,7 @@ export default function NavBar() {
                     >
                         <Link
                             to={`/categories/${category.id}`}
-                            className="text-muted-foreground hover:text-muted flex items-center justify-center px-3 py-2 font-semibold transition-colors duration-200 ease-in-out"
+                            className="text-muted-foreground hover:text-muted flex items-center justify-center px-4 py-1.5 font-semibold transition-colors duration-200 ease-in-out"
                         >
                             {category.name}
                         </Link>
