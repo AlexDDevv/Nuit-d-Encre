3e1582cf1cd6f6ce9d82615212f20f2e64e82d4c
Alexis <AlexDdevs@gmail.com>
2025-02-22 18:21:23 +0100

    💄 Remove styled-component to Tailwind

--- frontend/src/components/CategoryModal.tsx
+++ frontend/src/components/CategoryModal.tsx
@@ -2,97 +2,83 @@ import { useState } from "react";
 import { createCategory } from "../api/createCategory";
 import { useMutation } from "@apollo/client";
 import { queryCategories } from "../api/categories";
-import { Button } from "./StyledButton";
-import { Input } from "../components/styled/Form.styles";
-import styled from "styled-components";
-
-export const ModalForm = styled.div`
-    background-color: var(--popover);
-    border: 1px solid var(--border);
-    border-radius: 6px;
-    margin-top: 5px;
-    padding: 15px 20px;
-`;
-
-export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-`;
-
-export const Label = styled.label`
-    color: var(--popover-foreground);
-    font-size: 14px;
-`;
-
-export const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-
-    input {
-        width: 75%;
-    }
-`;
+import clsx from "clsx";
+import ActionButton from "./UI/ActionButton";
+import { useToast } from "./UI/Toaster/ToasterHook";
 
 export default function CategoryModal(props: {
     onCategoryCreated: (newId: number) => void;
 }) {
     const [name, setName] = useState("");
+    const [error, setError] = useState<boolean>(false);
+    const { addToast } = useToast();
 
-    const [doCreateCategory, { loading, error }] = useMutation<{
+    const [doCreateCategory] = useMutation<{
         createCategory: { id: number; name: string };
     }>(createCategory, { refetchQueries: [queryCategories] });
 
     const addCategory = async () => {
-        const category = await doCreateCategory({
-            variables: {
-                data: {
-                    name,
-                },
-            },
-        });
-        console.log(category);
+        try {
+            if (name.trim() === "") {
+                setError(true);
+                addToast("Le champ ne peut pas être vide.", "error");
+                return;
+            } else {
+                const category = await doCreateCategory({
+                    variables: {
+                        data: {
+                            name,
+                        },
+                    },
+                });
+                console.log(category);
 
-        setName("");
+                setName("");
 
-        const newCategoryId = category.data?.createCategory.id;
-        if (newCategoryId) {
-            props.onCategoryCreated(newCategoryId);
+                const newCategoryId = category.data?.createCategory.id;
+                if (newCategoryId) {
+                    props.onCategoryCreated(newCategoryId);
+                }
+            }
+        } catch (error) {
+            console.error("Une erreur est survenue :", error);
+            addToast("Une erreur est survenue.", "error");
         }
     };
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
-
     return (
-        <ModalForm>
-            <InputContainer>
-                <Label htmlFor="addCategory">
-                    Ajouter une nouvelle catégorie
-                </Label>
-                <Container>
-                    <Input
+        <div className="bg-popover border-border mt-1.5 rounded-md border px-5 py-4">
+            <div className="flex flex-col gap-1">
+                <label
+                    htmlFor="addCategory"
+                    className="text-foreground text-sm"
+                >
+                    Créer une nouvelle catégorie
+                </label>
+                <div className="flex items-center justify-center gap-2.5">
+                    <input
                         type="text"
                         id="addCtagory"
-                        placeholder="Ajouter une catégorie..."
+                        placeholder="Créer une catégorie..."
                         value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                            setError(false);
+                        }}
+                        className={clsx(
+                            "bg-input text-accent-foreground focus:outline-ring w-[60%] rounded-lg p-3 text-xs placeholder:italic placeholder:opacity-85 focus:outline-2",
+                            error && "border-destructive border",
+                        )}
                     />
-                    <Button
-                        minWidth="160px"
-                        width="25%"
-                        height="35px"
-                        transition="background-color 0.2s ease-in-out"
-                        backgroundHover="rgba(255, 204, 102, 0.9)"
-                        type="button"
+                    <ActionButton
+                        bgColor="bg-primary"
+                        color="text-primary-foreground"
+                        width="w-[40%]"
+                        content="Créer la catégorie"
                         onClick={addCategory}
-                    >
-                        Ajouter la catégorie
-                    </Button>
-                </Container>
-            </InputContainer>
-        </ModalForm>
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
--- frontend/src/components/TagModal.tsx
+++ frontend/src/components/TagModal.tsx
@@ -3,57 +3,74 @@ import { TagType } from "../../types";
 import { useMutation } from "@apollo/client";
 import { createTag } from "../api/createTag";
 import { queryTags } from "../api/tags";
-import { Button } from "./StyledButton";
-import { ModalForm, InputContainer, Label, Container } from "./CategoryModal";
-import { Input } from "../components/styled/Form.styles";
+import clsx from "clsx";
+import ActionButton from "./UI/ActionButton";
+import { useToast } from "./UI/Toaster/ToasterHook";
 
 export function TagModal(props: { onTagCreated: (newId: number) => void }) {
     const [name, setName] = useState("");
+    const [error, setError] = useState<boolean>(false);
+    const { addToast } = useToast();
 
     const [doCreateTag] = useMutation<{ createTag: TagType }>(createTag, {
         refetchQueries: [queryTags],
     });
-    async function doSubmit() {
+
+    const addTag = async () => {
         try {
-            const { data } = await doCreateTag({
-                variables: {
-                    data: {
-                        name,
+            if (name.trim() === "") {
+                setError(true);
+                addToast("Le champ ne peut pas être vide.", "error");
+                return;
+            } else {
+                const { data } = await doCreateTag({
+                    variables: {
+                        data: {
+                            name,
+                        },
                     },
-                },
-            });
-            setName("");
-            if (data) {
-                props.onTagCreated(data.createTag.id);
+                });
+                setName("");
+                if (data) {
+                    props.onTagCreated(data.createTag.id);
+                }
             }
-        } catch (err) {
-            console.error(err);
+        } catch (error) {
+            console.error("Une erreur est survenue :", error);
+            addToast("Une erreur est survenue.", "error");
         }
-    }
+    };
 
     return (
-        <ModalForm>
-            <InputContainer>
-                <Label>Ajouter un nouveau tag</Label>
-                <Container>
-                    <Input
+        <div className="bg-popover border-border mt-1.5 rounded-md border px-5 py-4">
+            <div className="flex flex-col gap-1">
+                <label htmlFor="addTag" className="text-foreground text-sm">
+                    Créer un nouveau tag
+                </label>
+                <div className="flex items-center justify-center gap-2.5">
+                    <input
                         type="text"
+                        id="addTag"
+                        placeholder="Créer un tag..."
                         value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                            setError(false);
+                        }}
+                        className={clsx(
+                            "bg-input text-accent-foreground focus:outline-ring w-2/3 rounded-lg p-3 text-xs placeholder:italic placeholder:opacity-85 focus:outline-2",
+                            error && "border-destructive border",
+                        )}
                     />
-                    <Button
-                        minWidth="160px"
-                        width="25%"
-                        height="35px"
-                        transition="background-color 0.2s ease-in-out"
-                        backgroundHover="rgba(255, 204, 102, 0.9)"
-                        type="button"
-                        onClick={doSubmit}
-                    >
-                        Créer mon tag
-                    </Button>
-                </Container>
-            </InputContainer>
-        </ModalForm>
+                    <ActionButton
+                        bgColor="bg-primary"
+                        color="text-primary-foreground"
+                        width="w-1/3"
+                        content="Créer le tag"
+                        onClick={addTag}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
