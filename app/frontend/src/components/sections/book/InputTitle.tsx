ecf1082f5607cbb1b12eeba5f67e656baac29974
Alexis <AlexDdevs@gmail.com>
2025-08-01 21:36:39 +0200

    Edit path

--- app/frontend/src/components/sections/book/InputTitle.tsx
+++ app/frontend/src/components/sections/book/InputTitle.tsx
@@ -1,34 +1,34 @@
-import { Input } from "@/components/ui/Input"
-import { Label } from "@/components/ui/Label"
-import { InputsProps } from "@/types/types"
+import { Input } from "@/components/UI/form/Input";
+import { Label } from "@/components/UI/form/Label";
+import { InputsProps } from "@/types/types";
 
 export default function InputTitle({ register, errors }: InputsProps) {
-	return (
-		<div>
-			<Label htmlFor="title" required>
-				Titre
-			</Label>
-			<Input
-				id="title"
-				type="text"
-				placeholder="Saisissez un titre pour votre enquête"
-				aria-required
-				{...register("title", {
-					required: "Le titre est requis",
-					minLength: {
-						value: 1,
-						message:
-							"Le titre doit contenir au moins un caractère.",
-					},
-					maxLength: {
-						value: 255,
-						message:
-							"Le titre doit contenir 255 caractères maximum.",
-					},
-				})}
-				aria-invalid={errors?.title ? "true" : "false"}
-				errorMessage={errors?.title?.message}
-			></Input>
-		</div>
-	)
+    return (
+        <div>
+            <Label htmlFor="title" required>
+                Titre
+            </Label>
+            <Input
+                id="title"
+                type="text"
+                placeholder="Saisissez un titre pour votre enquête"
+                aria-required
+                {...register("title", {
+                    required: "Le titre est requis",
+                    minLength: {
+                        value: 1,
+                        message:
+                            "Le titre doit contenir au moins un caractère.",
+                    },
+                    maxLength: {
+                        value: 255,
+                        message:
+                            "Le titre doit contenir 255 caractères maximum.",
+                    },
+                })}
+                aria-invalid={errors?.title ? "true" : "false"}
+                errorMessage={errors?.title?.message}
+            ></Input>
+        </div>
+    );
 }
