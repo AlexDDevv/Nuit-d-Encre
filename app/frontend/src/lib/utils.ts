32d18f5bd83b88df2f89e857ae673d85db4c3e75
Alexis <AlexDdevs@gmail.com>
2025-08-03 19:36:53 +0200

    Create util fonction for aria-label on BookCard component

--- app/frontend/src/lib/utils.ts
+++ app/frontend/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+export function buildAriaLabel(
+	title: string,
+	author: { firstname: string, lastname: string },
+	category = "Livre"
+) {
+	const authorName = `${author.firstname || ''} ${author.lastname || ''}`.trim();
+	return `Voir le livre ${title} par ${authorName} - ${category}`;
+}
