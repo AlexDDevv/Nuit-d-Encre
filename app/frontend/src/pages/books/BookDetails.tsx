4df7b1cc60bd8911475381b4ed690fbf1293f42d
Alexis <AlexDdevs@gmail.com>
2025-08-07 21:11:42 +0200

    WIP

--- app/frontend/src/pages/books/BookDetails.tsx
+++ app/frontend/src/pages/books/BookDetails.tsx
@@ -1,5 +1,6 @@
+import SelectBookState from "@/components/UI/SelectBookState";
 import { useBook } from "@/hooks/useBook";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function BookDetails() {
     const { slug } = useParams<{ slug: string }>();
@@ -11,11 +12,73 @@ export default function BookDetails() {
     const [idStr] = slug.split("-");
     const id = idStr;
 
-    const { book } = useBook(id)
+    const { book, bookLoading, bookError } = useBook(id)
+
+    if (bookLoading) {
+        return (
+            <div className="flex items-center justify-center">
+                <div>Chargement du livre...</div>
+            </div>
+        );
+    }
+
+    if (bookError) {
+        const isNotFoundError = bookError.graphQLErrors.some((error) =>
+            error.message.includes("Failed to fetch book"),
+        );
+
+        if (isNotFoundError) {
+            throw new Response("Book not found", { status: 404 });
+        }
+
+        // Pour les autres erreurs GraphQL
+        throw new Response("Error loading book", { status: 500 });
+    }
+
+    if (!book) {
+        throw new Response("Book not found", { status: 404 });
+    }
 
     console.log("🚀 ~ BookDetails ~ book:", book)
 
     return (
-        <div>BookDetails</div>
+        <div>
+            <div>
+                <div className="max-w-3xs max-h-96">
+                    <img src="/images/bookCover.svg" alt="Couverture d'un livre" className="w-full h-full" />
+                </div>
+                <div>
+                    <div>
+                        <h1>{book.title}</h1>
+                        <h2>{book.author.firstname} {book.author.lastname}</h2>
+                    </div>
+                    <div>
+                        {book.summary.length > 200 ? (
+                            <p>
+                                {book.summary.substring(
+                                    0,
+                                    200,
+                                )}
+                                ...
+                                <Link
+                                    to="#summary"
+                                    className="ml-1"
+                                >
+                                    Lire la suite
+                                </Link>
+                            </p>
+                        ) : (
+                            <p className="font-bodyFont text-foreground max-w-[650px] text-lg">
+                                {book.summary}
+                            </p>
+                        )}
+                    </div>
+                    <SelectBookState />
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
     )
 }
