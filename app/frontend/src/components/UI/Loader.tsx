074e082b33bcd255617f35d69596ff22337eb530
Alexis <AlexDdevs@gmail.com>
2025-08-10 08:48:41 +0200

    Loader minimalist

--- app/frontend/src/components/UI/Loader.tsx
+++ app/frontend/src/components/UI/Loader.tsx
@@ -1,15 +1,17 @@
+import "@/styles/loader.css"
+
 const Loader = () => {
     return (
         <div className="flex min-h-[70vh] items-center justify-center">
             <div className="book">
-                <div className="page turn"></div>
-                <div className="page turn"></div>
-                <div className="page turn"></div>
-                <div className="page turn"></div>
-                <div className="page turn"></div>
-                <div className="page turn"></div>
-                <div className="cover"></div>
-                <div className="cover turn"></div>
+                <div className="inner">
+                    <div className="left"></div>
+                    <div className="middle"></div>
+                    <div className="right"></div>
+                </div>
+                <ul>
+                    {Array.from({ length: 10 }, (_, i) => <li key={i}></li>)}
+                </ul>
             </div>
         </div>
     );
