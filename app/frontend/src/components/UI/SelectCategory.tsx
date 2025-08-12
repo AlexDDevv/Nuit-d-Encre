b5e8d314ac554dcfb5384fd8dd8a1852f4fdd1fc
Alexis <AlexDdevs@gmail.com>
2025-08-12 21:38:42 +0200

    Edit SelectCategory component to use Select component and query categories

--- app/frontend/src/components/UI/SelectCategory.tsx
+++ app/frontend/src/components/UI/SelectCategory.tsx
@@ -1,88 +1,65 @@
 import { useState } from "react";
-import { Check, ListFilter } from "lucide-react";
-import { motion, Variants, MotionProps, AnimatePresence } from "motion/react";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/UI/Select";
+import { cn } from "@/lib/utils";
+import { useBook } from "@/hooks/useBook";
+import Loader from "@/components/UI/Loader";
+import { categoryPropsType } from "@/types/types";
 
 export default function SelectCategory() {
-    const [showCategory, setShowCategory] = useState(false);
-    const [selectedCategory, setSelectedCategory] =
-        useState<string>("Toutes catégories");
+    const { categories, loadingCategories, errorCategories } = useBook()
+    const [selectedCategory, setSelectedCategory] = useState<string>("");
 
-    const handleCategories = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        setShowCategory(!showCategory);
-    };
+    if (loadingCategories) {
+        return (
+            <Loader />
+        );
+    }
 
-    const selectCategory = (category: string) => {
-        setSelectedCategory(category);
-        setShowCategory(!showCategory);
-    };
+    if (errorCategories) {
+        const isNotFoundError = errorCategories.graphQLErrors.some((error) =>
+            error.message.includes("Failed to fetch categories"),
+        );
 
-    const menuVariants: Variants = {
-        closed: {
-            scale: 0,
-            transition: {
-                delay: 0.15,
-            },
-        },
-        open: {
-            scale: 1,
-            transition: {
-                type: "spring",
-                duration: 0.4,
-                delayChildren: 0.2,
-                staggerChildren: 0.05,
-            },
-        },
-    };
+        if (isNotFoundError) {
+            throw new Response("Categories not found", { status: 404 });
+        }
 
-    const itemVariants: MotionProps = {
-        variants: {
-            closed: { x: -16, opacity: 0 },
-            open: { x: 0, opacity: 1 },
-        },
-        transition: { opacity: { duration: 0.2 } },
-    };
+        // Pour les autres erreurs GraphQL
+        throw new Response("Error loading categories", { status: 500 });
+    }
 
-    const categories = ["Toutes catégories", "Romans", "Mangas", "BD"];
+    if (!categories) {
+        throw new Response("Categories not found", { status: 404 });
+    }
+
+    const openStateClasses =
+        "data-[state=open]:ring-2 data-[state=open]:ring-ring data-[state=open]:ring-offset-2";
 
     return (
-        <div className="mx-auto flex w-[300px] flex-col gap-[10px]">
-            <button
-                className="bg-input border-border focus-visible:ring-ring flex w-full items-center justify-between rounded-lg px-4 py-2.5 focus-visible:ring-1 focus-visible:outline-none cursor-pointer"
-                onClick={handleCategories}
-            >
-                <p className="font-bodyFont text-accent-foreground text-sm">
-                    {selectedCategory}
-                </p>
-                <ListFilter className="text-accent-foreground h-4 w-4" />
-            </button>
-            <AnimatePresence>
-                {showCategory && (
-                    <motion.div
-                        className="bg-input w-full rounded-lg p-2.5"
-                        initial="closed"
-                        animate={showCategory ? "open" : "closed"}
-                        exit="closed"
-                        variants={menuVariants}
+        <Select value={selectedCategory} onValueChange={setSelectedCategory}>
+            <SelectTrigger className={cn(
+                "bg-input ring-offset-input text-accent-foreground focus-visible:ring-ring focus-within:ring-ring border-border flex w-full rounded-lg border px-3 py-2 text-sm placeholder:italic placeholder:opacity-85 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 max-w-80 mx-auto",
+                openStateClasses
+            )}>
+                <SelectValue placeholder="Sélectionnez une catégorie" />
+            </SelectTrigger>
+            <SelectContent animate={true}>
+                {categories.map((category: categoryPropsType) => (
+                    <SelectItem
+                        key={category.id}
+                        value={category.id}
+                        animate={true}
                     >
-                        {categories.map((category, i) => (
-                            <motion.div
-                                key={i}
-                                className={`mb-2.5 flex cursor-pointer items-center justify-between rounded px-2.5 py-2 transition-colors last:mb-0 ${selectedCategory === category ? "bg-accent" : "hover:bg-accent"}`}
-                                onClick={() => selectCategory(category)}
-                                {...itemVariants}
-                            >
-                                <p className="font-bodyFont text-accent-foreground text-sm">
-                                    {category}
-                                </p>
-                                {selectedCategory === category && (
-                                    <Check className="text-accent-foreground h-2.5 w-2.5" />
-                                )}
-                            </motion.div>
-                        ))}
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </div>
+                        {category.name}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
     );
 }
