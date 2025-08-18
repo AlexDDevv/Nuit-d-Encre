6fd0e9b0f499e237db37316cc9e9d8fe20c44081
Alexis <AlexDdevs@gmail.com>
2025-08-18 07:54:59 +0200

    Filter books on category

--- app/frontend/src/components/UI/SelectCategory.tsx
+++ app/frontend/src/components/UI/SelectCategory.tsx
@@ -6,7 +6,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/UI/Select";
-import { cn } from "@/lib/utils";
+import { cn, slugify } from "@/lib/utils";
 import { useBook } from "@/hooks/useBook";
 import Loader from "@/components/UI/Loader";
 import { categoryPropsType } from "@/types/types";
@@ -19,22 +19,25 @@ export default function SelectCategory() {
     )
     const [searchParams, setSearchParams] = useSearchParams()
 
-    const filterByCategory = (categorySlug: string, categoryId: string) => {
-        const newParams = new URLSearchParams(searchParams)
+    const filterByCategory = (categoryId: string) => {
+        const category = categories.find((category: categoryPropsType) => category.id === categoryId);
+        if (!category) return;
 
-        if (newParams.get("category") === categorySlug) {
-            newParams.delete("category")
-            newParams.delete("categoryId")
-            setSelectedCategory(null)
+        const slug = slugify(category.name);
+        const newParams = new URLSearchParams(searchParams);
+
+        if (newParams.get("category") === slug) {
+            newParams.delete("category");
+            newParams.delete("categoryId");
+            setSelectedCategory(null);
         } else {
-            newParams.set("category", categorySlug)
-            newParams.set("categoryId", categoryId)
-            setSelectedCategory(categorySlug)
+            newParams.set("category", slug);
+            newParams.set("categoryId", category.id);
+            setSelectedCategory(category.id);
         }
 
-        setSearchParams(newParams)
-    }
-    // filterByCategory(slug, category.id)
+        setSearchParams(newParams);
+    };
 
     if (loadingCategories) {
         return (
@@ -63,7 +66,7 @@ export default function SelectCategory() {
         "data-[state=open]:ring-2 data-[state=open]:ring-ring data-[state=open]:ring-offset-2";
 
     return (
-        <Select value={selectedCategory} onValueChange={setSelectedCategory}>
+        <Select value={selectedCategory ?? ""} onValueChange={filterByCategory}>
             <SelectTrigger className={cn(
                 "bg-input ring-offset-input text-accent-foreground focus-visible:ring-ring focus-within:ring-ring border-border flex w-full rounded-lg border px-3 py-2 text-sm placeholder:italic placeholder:opacity-85 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 max-w-60 mx-auto",
                 openStateClasses
