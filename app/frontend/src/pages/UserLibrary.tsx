2922a3bb7eca002226bdda1b2dc637f9a54a7bec
Alexis Delporte <alexis.delporte@likewatt.com>
2025-09-30 15:07:32 +0200

    WIP

--- app/frontend/src/pages/UserLibrary.tsx
+++ app/frontend/src/pages/UserLibrary.tsx
@@ -1,12 +1,22 @@
+import BookCardLibrary from "@/components/sections/book/BookCardLibrary";
 import { useUserBooksData } from "@/hooks/userBook/useUserBooksData";
+import { BookCardLibraryProps } from "@/types/types";
 
 export default function UserLibrary() {
     const { userBooks } = useUserBooksData({ mode: "library" });
-    console.log("🚀 ~ UserLibrary ~ userBooks:", userBooks);
 
     return (
-        <div>
-            <h1>Ma bibliothèque personnelle</h1>
+        <div className="flex flex-wrap items-center justify-center gap-20">
+            {userBooks.map((userBook: BookCardLibraryProps) => (
+                <BookCardLibrary
+                    key={userBook.id}
+                    id={userBook.id}
+                    book={userBook.book}
+                    rating={userBook.rating}
+                    recommended={userBook.recommended}
+                    status={userBook.status}
+                />
+            ))}
         </div>
     );
 }
--- app/frontend/src/types/types.ts
+++ app/frontend/src/types/types.ts
@@ -167,11 +167,6 @@ export interface AuthorInfoProps {
     author: Author;
 }
 
-export type BookStateItem = {
-    icon: React.ReactNode;
-    content: string;
-};
-
 export type GetCategoriesQuery = {
     categories: {
         id: string;
@@ -214,3 +209,32 @@ export interface CreateUserBookInput {
 }
 
 export type UserBookStatus = "TO_READ" | "READING" | "READ" | "PAUSED";
+
+export type UserBookStatusConfig = {
+    icon: LucideIcon;
+    label: string;
+    value: UserBookStatus;
+};
+
+export type SelectBookStatusProps = {
+    bookId: string;
+    status?: UserBookStatus;
+};
+
+export type BookCardLibraryProps = {
+    id: string;
+    book: {
+        title: string;
+        author: {
+            firstname: string;
+            lastname: string;
+        };
+        publishedYear: number;
+        publisher: string;
+        pageCount: number;
+        category: CategoryBook;
+    };
+    rating: number;
+    recommended: boolean;
+    status: UserBookStatus;
+};
