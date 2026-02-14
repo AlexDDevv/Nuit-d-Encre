f270319a91535a68657567dbe4a0989b87c3696c
Alexis <alexis.delporte@likewatt.com>
2026-02-14 20:07:03 +0100

    fix: remove all logic from UI component

--- app/frontend/src/components/sections/book/SelectBookStatus.tsx
+++ app/frontend/src/components/sections/book/SelectBookStatus.tsx
@@ -7,67 +7,23 @@ import {
 } from "@/components/UI/Select";
 import { cn } from "@/lib/utils";
 import { SelectBookStatusProps } from "@/types/types";
-import { useUserBookMutations } from "@/hooks/userBook/useUserBookMutations";
-import { useAuthContext } from "@/hooks/auth/useAuthContext";
-import { useToast } from "@/hooks/toast/useToast";
-import { Skeleton } from "../../UI/skeleton/Skeleton";
 import { useUserBookStatusMapping } from "@/hooks/userBook/useUserBookStatusMapping";
 import { BOOK_STATES, OPEN_STATE_CLASSES } from "@/constants/bookStatus";
 
 export default function SelectBookStatus({
-    bookId,
-    status,
+    value,
+    onChange,
+    disabled,
 }: SelectBookStatusProps) {
-    const { user } = useAuthContext();
-    const { showToast } = useToast();
-    const { createUserBook, isCreatingUserBook } = useUserBookMutations();
     const { labelToEnum, enumToLabel } = useUserBookStatusMapping();
-
-    const onAddBookToUserLibrary = async (label: string) => {
-        if (isCreatingUserBook || !user) {
-            if (!user) {
-                showToast({
-                    type: "error",
-                    title: "Non connecté",
-                    description:
-                        "Vous devez être connecté pour ajouter un livre à votre bibliothèque",
-                });
-            }
-            return;
-        }
-
-        const statusEnum = labelToEnum[label];
-
-        try {
-            await createUserBook({
-                bookId,
-                status: statusEnum,
-            });
-            showToast({
-                type: "success",
-                title: "Succès",
-                description:
-                    "Le livre a bien été ajouté à votre bibliothèque !",
-            });
-        } catch (error) {
-            showToast({
-                type: "error",
-                title: "Erreur",
-                description:
-                    "L'ajout du livre dans la bibliothèque de l'utilisateur a échoué...",
-            });
-        }
-    };
-
-    if (isCreatingUserBook) {
-        return <Skeleton className="h-10 w-60 rounded-lg" />;
-    }
 
     return (
         <Select
-            value={status ? enumToLabel[status] : ""}
-            onValueChange={onAddBookToUserLibrary}
-            disabled={isCreatingUserBook}
+            value={value ? enumToLabel[value] : ""}
+            onValueChange={(label) =>
+                onChange ? onChange(labelToEnum[label]) : undefined
+            }
+            disabled={disabled}
         >
             <SelectTrigger
                 className={cn(
