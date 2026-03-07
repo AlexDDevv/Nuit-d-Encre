29b403f516699641aae6237044b211a9ddffa0a4
Alexis <alexis.delporte@likewatt.com>
2026-03-07 17:40:30 +0100

    feat: new util function to setup color based on rating

--- app/frontend/src/lib/utils.ts
+++ app/frontend/src/lib/utils.ts
@@ -1,48 +1,53 @@
 import { Author, RequiredAuthorFields } from "@/types/types";
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
 
 export function buildBookAriaLabel(
-	title: string,
-	author: { firstname: string, lastname: string },
-	category = "Livre"
+    title: string,
+    author: { firstname: string; lastname: string },
+    category = "Livre",
 ) {
-	const authorName = `${author.firstname || ''} ${author.lastname || ''}`.trim();
-	return `Voir le livre ${title} par ${authorName} - ${category}`;
+    const authorName =
+        `${author.firstname || ""} ${author.lastname || ""}`.trim();
+    return `Voir le livre ${title} par ${authorName} - ${category}`;
 }
 
-export function buildAuthorAriaLabel(
-	firstname: string,
-	lastname: string
-) {
-	const authorName = `${firstname} ${lastname}`.trim();
-	return `Voir la fiche de l'auteur ${authorName}`;
+export function buildAuthorAriaLabel(firstname: string, lastname: string) {
+    const authorName = `${firstname} ${lastname}`.trim();
+    return `Voir la fiche de l'auteur ${authorName}`;
 }
 
 export function slugify(text: string): string {
-	return text
-		.normalize("NFD")
-		.replace(/[\u0300-\u036f]/g, "")
-		.toLowerCase()
-		.trim()
-		.replace(/[^a-z0-9\s-]/g, "")
-		.replace(/\s+/g, "-")
-		.replace(/-+/g, "-");
+    return text
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase()
+        .trim()
+        .replace(/[^a-z0-9\s-]/g, "")
+        .replace(/\s+/g, "-")
+        .replace(/-+/g, "-");
 }
 
 export const hasIncompleteInfo = (author: Author): boolean => {
-	const requiredFields: RequiredAuthorFields[] = [
-		'birthDate',
-		'nationality',
-		'wikipediaUrl',
-		'biography'
-	];
+    const requiredFields: RequiredAuthorFields[] = [
+        "birthDate",
+        "nationality",
+        "wikipediaUrl",
+        "biography",
+    ];
 
-	return requiredFields.some(field =>
-		!author[field] || author[field] === ""
-	);
+    return requiredFields.some(
+        (field) => !author[field] || author[field] === "",
+    );
+};
+
+export const getRatingClasses = (rating: number): string => {
+    if (rating >= 4) return "border-l-primary hover:border-primary";
+    if (rating === 3)
+        return "border-l-accent-foreground hover:border-accent-foreground";
+    return "border-l-destructive hover:border-destructive";
 };
